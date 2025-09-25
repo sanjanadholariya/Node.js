1a72495cbf791e6fb0ac67a3189fddcab60c82f1
KBthumar180 <kavya.thummar@gmail.com>
2025-09-25 17:56:20 +0530

    25/09/2025

--- adminPanel_category_passportjs/controller/subcategoryCtrl.js
+++ adminPanel_category_passportjs/controller/subcategoryCtrl.js
@@ -4,10 +4,19 @@ const categoryModel = require('../model/categoryModel')
 module.exports.addSubCategoryPage = async(req , res) => {
     try{
         let category = await categoryModel.find({})
-        console.log(category)
+        // console.log(category)
         return res.render('subcategory/addSubcategoryPage',{
             category
         })
+    }catch(err){
+        console.log(err)
+        return res.redirect('/admin')
+    }
+}
+
+module.exports.addSubcategory = async(req , res) => {
+    try{
+
     }catch(err){
         console.log(err)
         return res.redirect('/admin')
--- adminPanel_category_passportjs/routes/subcategoryRoutes.js
+++ adminPanel_category_passportjs/routes/subcategoryRoutes.js
@@ -3,8 +3,9 @@ const express = require('express')
 const routes = express.Router();
 
 const subcategoryModel = require('../model/subcategoryModel');
-const { addSubCategoryPage } = require('../controller/subcategoryCtrl');
+const { addSubCategoryPage, addSubcategory } = require('../controller/subcategoryCtrl');
 
 routes.get('/addSubCategoryPage',addSubCategoryPage)
+routes.post('/addSubcategory',addSubcategory)
 
 module.exports = routes;
