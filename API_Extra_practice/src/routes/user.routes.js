644f0961b22931c79107bdf84128a5d38e00b59e
KBthumar180 <kavya.thummar@gmail.com>
2025-10-13 10:31:59 +0530

    13/10/2025

--- API_Extra_practice/src/routes/user.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { allUsers } = require('../controller/user.controller');
-const verifyToken = require('../middleware/verifyToken')
-
-const routes = express.Router();
-
-routes.get('/',verifyToken,allUsers)
-
-module.exports = routes;
