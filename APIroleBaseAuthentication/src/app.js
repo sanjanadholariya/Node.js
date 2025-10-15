d623d4455f47a5dd0f0be75435d87e45fbe7df90
KBthumar180 <kavya.thummar@gmail.com>
2025-10-15 14:20:30 +0530

    15/10/2025

--- APIroleBaseAuthentication/src/app.js
+++ APIroleBaseAuthentication/src/app.js
@@ -5,11 +5,26 @@ const app = express();
 const port = 8001;
 
 const db = require('./config/db')
+const morgan = require('morgan')
+const cors = require('cors')
 
+// Middleware
+
+// cors == cross origin resource sharing
+// using this middleware (cors) , we can share cross platform at once like frontend and backend server at once
+app.use(cors({
+  origin : "attache_frontend_link_here"
+}))
+
+app.use(morgan('dev'))
 app.use(express.urlencoded())
 app.use(express.json())
 
+// Routes
+
 app.use('/api',require('./routes/index.routes'))
+
+// Server start
 
 app.listen(port , (err) => {
   err ? console.log(err) : console.log(`server is running on http://localhost:${port}`)
